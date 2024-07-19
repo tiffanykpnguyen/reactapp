f13d405bcbb52b777832dfac4643bcaa8f9951e4
tiffanykpnguyen <99049397+tiffanykpnguyen@users.noreply.github.com>
2024-07-18 20:17:56 -0500

    Update settings.py

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -132,7 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Frontend URL
-    "http://tiffany-demos3.s3-website.us-east-2.amazonaws.com/register",
 ]
 
 REST_FRAMEWORK = {
